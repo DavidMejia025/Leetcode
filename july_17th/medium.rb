b5fb64b39b756f673d8629e3c3253c42213e4b3a
david <davidndy@gmail.com>
2019-07-17 18:08:55 -0500

    try a new option

--- july_17th/medium.rb
+++ july_17th/medium.rb
@@ -105,7 +105,7 @@ def change_characters(a, index)
   a
 end
 
-a0 = ["a", "a", "a","b"]
+a0 = ["a", "a", "a","b","a"]
 a1 = ["a","a", "a","a", "a", "a","b", "b", "b", "b","c", "c", "c", "c","d", "d", "d", "d"]
 a2 = ["a", "a","a","a","b","b","b","c","c","c","d","d","d"]
 b =  ["b","b","b","b","b","b","b"]
@@ -116,6 +116,65 @@ b =  ["b","b","b","b","b","b","b"]
 #p reorganize_string(a2)
 #p reorganize_string(a1).count
 #p reorganize_string(a1)
-p reorganize_string(b)
-p reorganize_string(["b","b","b"])
-p reorganize_string(["b","b","b"])
+# p reorganize_string(b)
+# p reorganize_string(["b","b","b"])
+# p reorganize_string(["b","b","b"])
+#
+
+
+# Other alternative taking into account the following rule:
+# reorganize string is vaid if the number of different characters is less or equal to 1.
+# for example if s contains n  letters a and m leters b with n - p == m being p > 1 the string can not being reorganize
+# contrary    if s contains n  letters a and m leters b with n - p == m being 0 < p <  1 the string can be reorganize
+#
+# Pseudo code:
+# 1. sort array
+# 2. count the number of different elements
+# 3. verify rule
+#    if rule == true
+# 4. build new array
+#    else
+#      retun ""
+# end
+#
+#
+def reorganize_string(a)
+  a.sort!
+  p a
+  count = count_by_character(a)
+p count
+  can_reorganize?(count)
+end
+
+def can_reorganize?(count)
+  max = count.max
+  sum = count.reduce(&:+)
+
+  return false  if max == sum
+
+  max - (sum - max) <= 1
+end
+
+def count_by_character(a)
+  length_a = a.length
+
+  result = []
+  count  = 1
+  ref   = a[0]
+
+  length_a.times do |i|
+     if a[i] == a[i + 1]
+       count += 1
+     else
+       result.push(count)
+       ref = a[i+1]
+       count = 1
+     end
+  end
+
+  result
+end
+
+a0 = "cxmwmmm".split("")
+a1 = "ababa".split("")
+p reorganize_string(a1)
