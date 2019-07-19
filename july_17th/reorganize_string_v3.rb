d51c339380d302fac0cea65cab93672e303c8665
david <davidndy@gmail.com>
2019-07-19 13:39:05 -0500

    add two final solutions the last one pass let code evaluator

--- july_17th/reorganize_string_v3.rb
+++ july_17th/reorganize_string_v3.rb
@@ -20,54 +20,113 @@
 #return resul
 
 def reorganize_string(s)
+ result   = []
 
- result   = ""
- length_s = s.length
-
- until s.empty? do # im using so much .nil? just because .present didnt work :/
-   length_s.times do |index|
-p "result"
-p result
-     new_char = check_neighboor(s, new_char, index)
-
-     result  += new_char unless new_char.nil?
-     p result
-   end
+ until s.empty? do
+  result =  add_one_character(s, result)
+  break if result == [""]
  end
 
  result
 end
 
-def check_neighboor(s, new_char, index)
-p "!"
-p index
-p s
-p s[index]
-p s[index -1]
-p"!"
+# Manual test add_one_character
+# xwvmm
+# s         mm
+# result   xwv
+# s[0]     m
+# ch       x
+# i        0
+# index    0
+# new_char m
+# result   x.push(m) + wv
+#
+# s m
+# result   xmwv
 
-   return s.shift  if (index == 0) || (s[0] != s[1])
 
-   s.each do |index, ch|
-     return index if s[0] != ch && s[0] != s[index + 1]
-   end
+def add_one_character(s, result)
+  return result.push(s.shift)  if (s.length != 1) && (s[0] != s[1])
 
-   new_char = s.pop(index)
-   p new_char
-   new_char
+  index = -1
+
+  result.each_with_index do |ch, i|
+    return result.unshift(s.shift) if i == 0 && s[0] != ch
+
+    index = i if s[0] != ch && s[0] != result[i + 1]
+  end
+
+  return [""] if index == -1
+
+  new_char = s.shift
+
+  return result[0..index].push(new_char) + result[index + 1..-1]
 end
+def adjacent_chars(str)
+  result = ""
+
+  remaining = str.split("")
+  until remaining.empty?
+    at_least_one_removed = false
+    remaining.delete_if do |c|
+      pos = find_valid_position(result, c)
+      if pos != -1
+        result.insert(pos, c)
+        at_least_one_removed = true
+        true # delete char
+      else
+        false # don't delete char
+      end
+    end
+
+    return "" unless at_least_one_removed
+  end
+
+  return result
+end
+
+def find_valid_position(str, c)
+  return 0 if str.empty?
+
+  for i in 0...str.length
+    if i == 0 && str[0] != c
+      return 0
+    elsif i == str.length - 1 && str[i] != c
+      return str.length
+    elsif str[i] != c && str[i+1] != c
+      return i + 1
+    end
+  end
+
+  return -1
+end
+
+
+
+
 
 a0 = "cxmwmmm".split("")
 a1 = "ababa".split("")
 a2 = "baaba".split("")
-a3 = "vvvlo".split("")
+a3 = "vvvlo".split("") # fucked another boring edge case.
 a4 = "aaab".split("")
 a5 = "bfrbs".split("")
 a6 = "todrnphcamnomskfrhe".split("")
-p  reorganize_string(a0)
+#  reorganize_string(a0)
 #  reorganize_string(a1)
 #  reorganize_string(a2)
 #  reorganize_string(a3) #pending due to merge sort update for non n^2 array length
 #  reorganize_string(a4)
 #  reorganize_string(a5)
 #  reorganize_string(a6)
+
+p adjacent_chars("bbbbaaaaababaababab")
+p adjacent_chars("aaab")
+
+p adjacent_chars(a0.join)
+p adjacent_chars(a1.join)
+p adjacent_chars(a2.join)
+p adjacent_chars(a3.join)
+p adjacent_chars(a4.join)
+p adjacent_chars(a5.join)
+p adjacent_chars(a6.join)
